33db5c4e6909ec195c8ceed03d34317074ed4d10
bhardwajVishal1 <thegreatvishal80@gmail.com>
2026-08-17 15:05:28 +0530

    Add files via upload

---
+++ Assignment_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        sum = sum + num ; 
+    }
+
+    int total = (n + 1) * (n + 2) / 2;
+
+    cout << total - sum;
+
+    return 0;
+}
